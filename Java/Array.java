15dc973da1b7d91b8d3470cca19f74ce71061fbc
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-08-30 01:47:03 +0530

    java programming

---
+++ Java/Array.java
@@ -0,0 +1,17 @@
+class Array{
+	public static void main(String args[]){
+		
+		int a[] = new int[5];
+		a[0] = 10;
+		a[1] = 11;
+		a[2] = 12;
+		a[3] = 13;
+		a[4] = 14;
+		
+	
+
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+			System.out.println(a.length);
+	}
+}
