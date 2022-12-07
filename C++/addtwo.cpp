e07d1b8e8dab091ee9615e7699950122459e88ac
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-12-07 21:26:37 +0530

    addtwo int

---
+++ C++/addtwo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  
+  int x,y, sum;
+
+  cout <<" Enter the first integer number: ";
+  cin >> x;
+  cout <<"Enter the second integer number: ";
+  cin >> y;
+
+  sum = x+y;
+
+  cout <<" Sum of integer number: " << sum;
+
+  return 0;
+}
+
