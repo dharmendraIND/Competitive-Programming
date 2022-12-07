ccff7d63e3c09ba32af75b76787e1b0f91c32c06
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-12-07 22:24:59 +0530

    unfinish

---
+++ C++/question/printnum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int number;
+
+  cout <<" enter an integer:";
+
+  cin>> number;
+
+  cout << "yoou entered "<< number;
+  return 0;
+
+}
+
+
---
+++ C++/question/sizeoftype.cpp
@@ -0,0 +1,9 @@
+// find size of a variable
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+  cout<< "size of char: "<< sizeof(char) << "byte"<< endl;
+
