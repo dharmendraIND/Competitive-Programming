0b7cd98cbbc977b0dbee39d096385e8ced347912
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-12-07 21:31:19 +0530

    another way

---
+++ C++/addint.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+  int first, second, sum;
+
+  cout << "Enter two integer number: ";
+  cin >> first >> second;
+
+  sum = first + second;
+
+  cout << first << " + " << second<< "=" << sum;
+
+  return 0;
+
+}
