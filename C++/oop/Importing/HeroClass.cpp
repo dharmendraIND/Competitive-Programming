2e5b7237b72ed58c23041154785df1144440826b
Dharmendra Kumar <57281417+DharmendraGITHB@users.noreply.github.com>
2022-12-12 09:29:52 +0530

    importing files

---
+++ C++/oop/Importing/HeroClass.cpp
@@ -0,0 +1,8 @@
+class Hero
+{ 
+	char name[100];
+	int health;
+	char level;
+
+};
+
---
+++ C++/oop/Importing/ImportingHero.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "HeroClass.cpp"
+using namespace std;
+
+int main()
+{
+	Hero h2;
+
+	cout << "size: "<< sizeof(h2) << endl;
+
+	return 0;
+}
